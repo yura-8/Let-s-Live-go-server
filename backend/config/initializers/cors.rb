12889b30aee2acd43bbc0ce0a072fd39d076617f
mahiro1111 <148053248+mahiro1111@users.noreply.github.com>
2024-06-08 17:03:48 +0900

    corserror

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -14,3 +14,15 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'localhost:3001' #ReactのサーバーのURLを設定してください。
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :delete, :options, :head],
+        credentials: true
+    end
+  end
+  
+  
